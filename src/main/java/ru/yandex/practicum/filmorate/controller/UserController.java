0ec41f59126b5dbe1e5e3349a25c6ae6b264466a
Алексей <alexeizubtsoff@yandex.ru>
2024-06-21 12:13:32 +0300

    1) Added a custom annotation `NotBeforeDate`, which checks that the film release date is not earlier than December 28, 1985. 2) Added logging with `@Slf4j`. 3) Created classes `UserController` and `FilmController` to handle the endpoints `/users` and `/films`.

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,68 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+    Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> getAllUsers() {
+        log.info("Get all users");
+        return users.values();
+    }
+
+    @PostMapping
+    public User createUser(@Valid @RequestBody User user) {
+        log.trace("Create user: {}", user);
+        if (user.getName() == null || user.getName().isEmpty()) {
+            log.info("Name null ? -> Name = login");
+            user.setName(user.getLogin());
+        }
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        log.info("Created new user: {}", user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@Valid @RequestBody User user) {
+        log.trace("Update user: {}", user);
+        if (user.getId() == null) {
+            log.warn("Id can not be null");
+            throw new ValidationException("User id is null");
+        }
+        if (users.containsKey(user.getId())) {
+            User olduser = users.get(user.getId());
+            olduser.setName(user.getName());
+            olduser.setBirthday(user.getBirthday());
+            olduser.setLogin(user.getLogin());
+            olduser.setEmail(user.getEmail());
+            log.info("Updated user: {}", olduser);
+            return olduser;
+        }
+        log.warn("User not found");
+        throw new ValidationException("User not found");
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        log.trace("Current maxId: {}", currentMaxId);
+        return ++currentMaxId;
+    }
+}
