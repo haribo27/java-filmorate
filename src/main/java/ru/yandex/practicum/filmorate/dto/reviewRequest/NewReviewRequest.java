f7444bf40654bb2dc74dbba4689f02d2463b0e9c
haribo52 <alexeizubtsoff@yandex.ru>
2024-08-20 13:00:24 +0400

    refactor code

--- src/main/java/ru/yandex/practicum/filmorate/dto/reviewRequest/NewReviewRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/reviewRequest/NewReviewRequest.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.dto.reviewRequest;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
