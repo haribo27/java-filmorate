f5a5e07c38da27c26fd3e07a2e9b076b678cbe80
Valera <108022376+kekobka@users.noreply.github.com>
2024-08-21 11:58:25 +0700

    remove unused variables

--- src/main/java/ru/yandex/practicum/filmorate/dal/EventFeedRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/EventFeedRepository.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.dal.mappers.EventFeedMapper;
 import ru.yandex.practicum.filmorate.model.EventFeed;
 
 import java.sql.Timestamp;
@@ -16,11 +15,8 @@ import java.util.List;
 @Repository("EventFeedRepo")
 public class EventFeedRepository extends BaseRepository<EventFeed> implements EventFeedStorage {
 
-    EventFeedMapper extractor;
-
-    public EventFeedRepository(JdbcTemplate jdbc, RowMapper<EventFeed> mapper, EventFeedMapper extractor) {
+    public EventFeedRepository(JdbcTemplate jdbc, RowMapper<EventFeed> mapper) {
         super(jdbc, mapper);
-        this.extractor = extractor;
     }
 
     private static final String FIND_BY_USER_ID_QUERY = "SELECT * FROM events_feed WHERE user_id = ?";
