b85f01ace122f57cd4df49cd84c66e5b1ef2e647
Ilya Starikov <lucky979star@yandex.ru>
2024-08-24 17:51:01 +0500

    fix for checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -65,7 +65,7 @@ public class FilmController {
         filmService.addFilmLike(id, userId);
     }
 
-    // изменения в БД schema.sql
+    // изменения в schema.sql
     @DeleteMapping("/{id}")
     public void deleteFilm(@PathVariable long id) {
         filmService.deleteFilm(id);
