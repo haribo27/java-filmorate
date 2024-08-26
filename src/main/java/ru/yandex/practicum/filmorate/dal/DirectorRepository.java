5e3e2080887714f29f4c62931366aa97dd60ccb6
haribo52 <alexeizubtsoff@yandex.ru>
2024-08-26 06:30:58 +0400

    remove unused methods from DirectorRepository.java

--- src/main/java/ru/yandex/practicum/filmorate/dal/DirectorRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/DirectorRepository.java
@@ -5,7 +5,6 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Director;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
